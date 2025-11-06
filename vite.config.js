c1c72b6e46740bfa1860bfaa1303a35931b1172d
Hugo Black <hugovieira.eng@gmail.com>
2025-11-06 00:03:58 -0300

    atualizacao de estrutura de codigo

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
+import { URL, fileURLToPath } from 'node:url'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
