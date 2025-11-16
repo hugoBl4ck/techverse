81f050645f2ada2ed9d9c65b0a993d36df9cd020
Hugo Black <hugovieira.eng@gmail.com>
2025-11-16 13:05:50 -0300

    correcao caminho imagens

--- vite.config.js
+++ vite.config.js
@@ -35,6 +35,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
           {
